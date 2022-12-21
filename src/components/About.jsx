69522507e50570e58d68b9c10584752ea5b0335b
Lionel Cancellieri <lio2014vt@gmail.com>
2022-12-21 10:16:27 -0300

    prueba en about:notebook

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export const About = () => {
       <h1 className='heading animate__animated animate__bounce'>SOBRE MÍ</h1>
 
       <h2 className='color-text-global'>
-        Hola mi nombre es <strong className='color-strong'> Lionel Cancellieri </strong> , soy Técnico Superior en Desarrollo de Software y un apasionado de la <strong className='color-strong'> informatica </strong> así como también del mundo de la <strong className='color-strong'> programación </strong> . Tengo como objetivo ser un gran profesional y me capacito continuamente para poder lograrlo, me caracterizo por ser autodidacta, proactivo y sumamente responsable.
+        Hola mi nombre es <strong className='color-strong'> Lionel Cancellieri </strong> , soy Técnico Superior en Desarrollo de Software y un apasionado de la <strong className='color-strong'> informatica </strong> así como también del mundo de la <strong className='color-strong'> programación </strong> . Tengo como objetivo ser un gran profesional y me capacito continuamente para poder lograrlo, me caracterizo por ser autodidacta, proactivo y sumamente responsable. PRUEBAA
       </h2>
 
       <Footer />

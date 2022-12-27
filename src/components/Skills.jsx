1476cb518bd0e9472c3d916de5ac5ebe0d08a7b4
Lionel Cancellieri <lio2014vt@gmail.com>
2022-12-27 12:54:28 -0300

    updated 27/12/22 12:54hs: NOTEBOOK

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ export const Skills = () => {
 
       <h1 className='heading heading animate__animated animate__bounceIn'>SKILLS</h1>
 
-      <section className='syles-cards-skills d-xl-none mt-4'>
+      <section className='d-block d-sm-none mt-4'>
 
         <div>
           <img src="../assets/imgs/html-5.png" className="card-img-top" alt="..." />
@@ -121,65 +121,66 @@ export const Skills = () => {
           <div className="carousel-item">
             <img src="../assets/imgs/html-5.png" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='animate__animated animate__fadeInLeft'>HTML 5</h5>
-              <p className='animate__animated animate__fadeInRight'>Lenguaje de etiquetas, utilizado para la estructuración y la presentación de contenido en los sitios web.</p>
             </div>
+            <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> HTML 5 </strong> </h5>
+            <p className='text-center pb-5 animate__animated animate__fadeInRight'>Lenguaje de etiquetas, utilizado para la estructuración y <br /> la presentación de contenido en los sitios web.</p>
+            
           </div>
 
           <div className="carousel-item active">
             <img src="../assets/imgs/react.jpg" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='animate__animated animate__fadeInLeft'>REACT JS</h5>
-              <p className='animate__animated animate__fadeInRight'> Una biblioteca de JavaScript para construir interfaces de usuario. </p>
             </div>
+            <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> REACT JS </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Una biblioteca de JavaScript para construir <br /> interfaces de usuario. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/javascript.webp" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='text-black animate__animated animate__fadeInLeft' >JAVASCRIPT</h5>
-              <p className='text-black animate__animated animate__fadeInRight'> Lenguaje de programación que los desarrolladores utilizan para hacer páginas web interactivas. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft' > <strong> JAVASCRIPT </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Lenguaje de programación que los desarrolladores utilizan <br /> para hacer páginas web interactivas. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/css.png" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='animate__animated animate__fadeInLeft'>CSS 3</h5>
-              <p className='animate__animated animate__fadeInRight'> lenguaje de marcado que se emplea para dar formato a un sitio web. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> CSS 3 </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> lenguaje de marcado que se emplea para <br /> dar formato a un sitio web. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/bootstrap.jpg" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='text-black animate__animated animate__fadeInLeft'>BOOTSTRAP</h5>
-              <p className='text-black m-auto w-75 mt-2 animate__animated animate__fadeInRight'> Framework CSS y Javascript para la creación de interfaces y diseño responsive. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> BOOTSTRAP </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Framework CSS y Javascript para la creación de <br /> interfaces y diseño responsive. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/node-js.jpg" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='text-white animate__animated animate__fadeInLeft'>NODE JS</h5>
-              <p className='text-white m-auto w-75 mt-2 animate__animated animate__fadeInRight'> Entorno en tiempo de ejecución multiplataforma para la capa del servidor basado en JavaScript. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> NODE JS </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Entorno en tiempo de ejecución multiplataforma <br /> para la capa del servidor basado en JavaScript. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/git.png" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='text-white animate__animated animate__fadeInLeft'>GIT</h5>
-              <p className='text-white m-auto w-75 mt-2 animate__animated animate__fadeInRight'> Sistema de control de versiones que nos permitirá trabajar de manera colaborativa. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> GIT </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Sistema de control de versiones que nos <br /> permitirá trabajar de manera colaborativa. </p>
           </div>
 
           <div className="carousel-item">
             <img src="../assets/imgs/mongo-db.jpg" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
             <div className="carousel-caption d-none d-md-block">
-              <h5 className='text-white animate__animated animate__fadeInLeft'>MONGO DB</h5>
-              <p className='text-white m-auto w-75 mt-2 animate__animated animate__fadeInRight'> Es un sistema de base de datos NoSQL orientado a documentos de código abierto y escrito en C++. </p>
             </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> MONGO DB </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> Es un sistema de base de datos NoSQL orientado <br /> a documentos de código abierto y escrito en C++. </p>
           </div>
 
         </div>

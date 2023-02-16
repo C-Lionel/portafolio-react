d1b800c985766eb218386fa2e77195b7537048ab
lionel cancellieri <lio2014vt@gmail.com>
2023-02-16 02:13:43 -0300

    updated 16/2/23 2:15hs PC

--- src/data/works.js
+++ src/data/works.js
@@ -15,7 +15,21 @@ export const works = [
         'technologies': 'HTML 5, CSS 3, JAVASCRIPT, BOOSTRAP',
         'categories': 'Aplicación web',
         'description': 'Aplicación creada con vanilla Javascript moderno, simulando el famoso juego del backjack'
+    }, 
+    {
+        'id': 'app-journal',
+        'name': 'Journal-app',
+        'url': 'journal-application-react.netlify.app/',
+        'technologies': 'HTML 5, CSS 3, JAVASCRIPT, BOOSTRAP, REACT, REDUX, REDUX-TOOLKIT, MATERIAL UI, FIREBASE, CLOUDINARY',
+        'categories': 'Aplicación web',
+        'description': 'Aplicación creada con react utilizando un sistema de rutas, redux para manejar la información, firebase a fin de la autenticación mas las bases de datos y cloudinary para la subida de imagenes'
+
     }
+    
+    
+        
+     
+   
 
 
 ];

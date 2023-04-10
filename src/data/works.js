c7e637260d539b044c80e94a2663d1f878208225
lionel cancellieri <lio2014vt@gmail.com>
2023-04-10 16:57:32 -0300

    updated

--- src/data/works.js
+++ src/data/works.js
@@ -24,6 +24,15 @@ export const works = [
         'categories': 'Aplicación web',
         'description': 'Aplicación creada con react utilizando un sistema de rutas, redux para manejar la información, firebase a fin de la autenticación mas las bases de datos y cloudinary para la subida de imagenes'
 
+    },
+    {
+        'id': 'app-calendar',
+        'name': 'calendar-app',
+        'url': 'app-calendar-mern.netlify.app/auth/login',
+        'technologies': 'HTML 5, CSS 3, JAVASCRIPT, BOOSTRAP, REACT, REDUX, REDUX-TOOLKIT, MATERIAL UI, FIREBASE, CLOUDINARY, MONGO DB',
+        'categories': 'Aplicación web',
+        'description': 'Aplicación creada con con el stack MERN'
+
     }
     
     

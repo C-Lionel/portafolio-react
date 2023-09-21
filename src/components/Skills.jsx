d6acb9f3ff8dea6ebca378a3689db904c4a4c1fe
Lionel Cancellieri <lio2014vt@gmail.com>
2023-09-21 11:18:12 -0300

    UPDATED: NOTEBOOK

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ export const Skills = () => {
 
       <h1 className='heading heading animate__animated animate__bounceIn'>SKILLS</h1>
 
-      <section className='d-block d-sm-none mt-4'>
+      {/* <section className='d-block d-sm-none mt-4'>
 
         <div>
           <img src="../assets/imgs/html-5.png" className="card-img-top" alt="..." />
@@ -97,9 +97,18 @@ export const Skills = () => {
 
         <br />
 
+        <div>
+          <img src="../assets/imgs/angular.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <br />
+            <h5 className="card-title text-center"> <strong>  {'ANGULAR'} </strong> </h5>
+            <p className="card-text text-center">AngularJS es un popular framework de desarrollo web JavaScript que fue desarrollado por Google. Se utiliza para crear aplicaciones web dinámicas y de una sola página (SPA, por sus siglas en inglés). AngularJS ha sido reemplazado por Angular (también conocido como Angular 2+), que es una reescritura completa de la versión original.</p>
+          </div>
+        </div>
 
+        <br />
 
-      </section>
+      </section> */}
 
 
       {/* <img src="./src/assets/imgs/contacto.jpg" alt="hola" /> */}
@@ -114,6 +123,7 @@ export const Skills = () => {
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 6"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 7"></button>
           <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="7" aria-label="Slide 8"></button>
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="8" aria-label="Slide 9"></button>
         </div>
 
         <div className="carousel-inner">
@@ -173,6 +183,14 @@ export const Skills = () => {
             </div>
               <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> GIT </strong> </h5>
               <p className='text-center pb-5 animate__animated animate__fadeInRight'> Sistema de control de versiones que nos <br /> permitirá trabajar de manera colaborativa. </p>
+          </div>
+
+          <div className="carousel-item">
+            <img src="../assets/imgs/angular.jpg" className="d-block w-50 m-auto rounded-2 animate__animated animate__fadeInLeft" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+            </div>
+              <h5 className='text-center pt-4 animate__animated animate__fadeInLeft'> <strong> ANGULAR </strong> </h5>
+              <p className='text-center pb-5 animate__animated animate__fadeInRight'> AngularJS es un popular framework de desarrollo web JavaScript que fue desarrollado por Google. <br /> Se utiliza para crear aplicaciones web dinámicas yde una sola página (SPA, por sus siglas en inglés).</p>
           </div>
 
           <div className="carousel-item">
